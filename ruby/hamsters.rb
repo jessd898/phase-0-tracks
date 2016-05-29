a52b237948e1294b181da5b121570d10770e63b3
Jessica Dellert <jessica.dellert@gmail.com>
2016-05-29 13:14:25 -0400

    Added questions and output for 4.3

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -17,7 +17,6 @@ puts "What is the approximate age of this hamster?"
     else 
         age = age.to_f
     end
-    
 
 puts "The hamster is named #{name}, and is #{volume} notches loud on a scale of 1-10."
 puts "The hamster is #{fur_color} and approximately #{age} years old."
