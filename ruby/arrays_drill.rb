02b54f0df5464d5169fd4af8c5b26b700b182ba7
Jessica Dellert <jessica.dellert@gmail.com>
2016-06-02 20:05:42 -0400

    Complete Release 1

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -6,3 +6,18 @@ p hobbies_array
 
 hobbies_array.delete_at(2)
 p hobbies_array
+
+hobbies_array.insert(2, "running")
+p hobbies_array
+
+hobbies_array.delete("yoga")
+p hobbies_array
+
+hobbies_array.include?("knitting")
+p "Do you like knitting (true/false)? " + hobbies_array.include?("knitting").to_s
+
+color_array = ["blue", "yellow", "red"]
+p color_array
+
+new_array = hobbies_array + color_array
+p new_array
