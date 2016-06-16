7deb890414494c66a7c2d5ee8c6fce6bc505804c
Jessica Dellert <jessica.dellert@gmail.com>
2016-06-16 00:18:41 -0400

    Complete release 4

---
+++ js/explore.js
@@ -0,0 +1,19 @@
+// take a string variable 
+// split every letter up into an array
+// reverse the order of the array
+// combine back into one string 
+// "test".split(',');
+// if hello is hello than print the answer 
+
+
+function reverse(str) {
+  return str.split('').reverse().join('');
+}
+
+
+
+answer = reverse("Jessi"); 
+if ("hello"=="hello")
+{
+  console.log(answer)
+}
